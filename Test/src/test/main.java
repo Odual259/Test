a34ff655b6a7b41274654a47d5454b9ec6d37376
Odual259 <obonilla10327@ufide.ac.cr>
2023-02-26 18:56:05 -0600

    Update main.java
    Cambio

--- Test/src/test/main.java
+++ Test/src/test/main.java
@@ -16,7 +16,7 @@ public class main {
     public static void main(String[] args) {
         // TODO code application logic here
         /*Hola*/
-        //Hola Odual
+        //Hola Odual*
     }
     
 }
